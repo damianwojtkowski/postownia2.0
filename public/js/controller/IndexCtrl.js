48edc6169d40efa0ce5f18de6957bcdf3c39020f
Damian Wojtkowski <wojtkowskidamian@gmail.com>
2017-06-28 22:39:55 +0200

    User can add new post

--- public/js/controller/IndexCtrl.js
+++ public/js/controller/IndexCtrl.js
@@ -1,6 +1,17 @@
 angular.module('postownia').controller('IndexCtrl', ['$http', function ($http) {
   var vm = this;
+  vm.displayPosts = function () {
   $http.get('/posts').then(function (res) {
     vm.posts = res.data;
   });
+}
+  vm.addPost = function () {
+    var data = {post: vm.content};
+    console.log(data);
+    $http.post('/newpost', data).then(function (res) {
+      vm.content = '';
+      vm.displayPosts();
+    });
+  }
+  vm.displayPosts();
 }]);
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var fs = require('fs');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/posts', function (req, res) {
-  fs.readFile('posts.json', function(err, content) {
+  fs.readFile('posts.json', function (err, content) {
     if (err) {
       return res.send(err);
     }
@@ -16,6 +18,28 @@ app.get('/posts', function (req, res) {
 // app.get('*', function (req, res) {
 //   res.sendFile('index.html', {root: __dirname + '/public'});
 // });
+
+app.post('/newpost', function (req, res) {
+  fs.readFile('posts.json', function (err, content) {
+    if (err) {
+      return res.send(err);
+    }
+    var json = JSON.parse(content);
+    var obj = req.body;
+    var content = obj.post;
+    var date = new Date();
+    var post = {
+      user: 'Damian',
+      postdate: date,
+      content: content,
+      postid: date.getTime()
+    }
+    json.push(post);
+    var file = JSON.stringify(json);
+    fs.writeFileSync('posts.json', file)
+    return res.send()
+  });
+});
 
 app.get('/', function (req, res) {
 	res.send('index.html');
